3338e36336097d427e3ea1e3e479c50ab8267e1f
Santiagos-Vertigo <dgo.narvaez@gmail.com>
2024-03-09 22:12:27 -0600

    folders

---
+++ front-end/src/pages/LogInPage.js
@@ -0,0 +1,14 @@
+export const LogInPage = () => {
+    return (
+        <div clasName="content-container">
+            <h1>Log In</h1>
+            <input 
+                placeholder="someone@gmail.com"/>
+            <input 
+                tyoe="password"
+                placeholder="password"/>
+            <button>Log In</button>
+
+        </div>
+    )
+}cd
